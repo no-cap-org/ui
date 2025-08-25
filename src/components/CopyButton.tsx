3b8f28eace902e559cf93bf44be42438134d4764
i-supermario <sarang1699@gmail.com>
2025-08-25 10:32:36 -0700

    add copy link button

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,35 @@
+import { toast } from "sonner";
+import { Button } from "./ui/button";
+import { CopyCheckIcon, CopyIcon } from "lucide-react";
+import { useState } from "react";
+
+interface PropsI {
+  link: string
+}
+
+export default function CopyLinkButton({ link }: PropsI ) {
+
+  const [isCopied, setIsCopied ] = useState<boolean>(false);
+
+  return (
+    <>
+      <Button
+        variant="link"
+        onClick={() => {
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false),3000);
+          navigator.clipboard.writeText(
+            link
+          );
+          toast.success("Group join link copied to clipboard");
+        }}
+      >
+        {
+          isCopied ?
+          <CopyCheckIcon color="green"/> : <CopyIcon/>
+        }
+      </Button>
+    </>
+  )
+
+}
